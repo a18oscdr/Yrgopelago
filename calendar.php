9075de1f05d64df474c65f491d26b8137df39a97
Oscar Drabik <oscar.drabik@gmail.com>
2025-01-16 18:40:55 +0100

    Final version of calendar.php

--- calendar.php
+++ calendar.php
@@ -1,40 +1,4 @@
 <?php
-$year = 2025;
-$month = 1;
-
-$daysInMonth = date('t', mktime(0, 0, 0, $month, 1, $year));
-
-echo "January $year\n";
-echo "Mo Tu We Th Fr Sa Su\n";
-
-$dayOfWeek = date('w', mktime(0, 0, 0, $month, 1, $year));
-for ($i = 0; $i < $dayOfWeek; $i++) {
-    echo "   ";
-}
-
-for ($day = 1; $day <= $daysInMonth; $day++) {
-    echo sprintf("%2d ", $day);
-    if (($day + $dayOfWeek - 1) % 7 == 0) {
-        echo "\n";
-    }
-}
-function displayBookings($arrivalDate, $departureDate) {
-    // Connect to the database
-    $db = new mysqli("localhost", "username", "password", "database");
-
-    // Get the bookings for the given date range
-    $query = "SELECT * FROM bookings WHERE arrival_date <= '$departureDate' AND departure_date >= '$arrivalDate'";
-    $result = $db->query($query);
-
-    // Display the bookings
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo "Room " . $row["room_id"] . " booked from " . $row["arrival_date"] . " to " . $row["departure_date"] . "\n";
-        }
-    } else {
-        echo "No bookings found for the given date range.\n";
-    }
-}
 function displayCalendar($year, $month) {
     // Get the number of days in the month
     $daysInMonth = date('t', mktime(0, 0, 0, $month, 1, $year));
@@ -54,4 +18,28 @@ function displayCalendar($year, $month) {
         }
     }
 }
+
+function displayBookings($arrivalDate, $departureDate) {
+    // Connect to the database
+    $db = new mysqli("localhost", "username", "password", "database");
+
+    // Get the bookings for the given date range
+    $query = "SELECT * FROM bookings WHERE arrival_date <= '$departureDate' AND departure_date >= '$arrivalDate'";
+    $result = $db->query($query);
+
+    // Display the bookings
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "Room " . $row["room_id"] . " booked from " . $row["arrival_date"] . " to " . $row["departure_date"] . "\n";
+        }
+    } else {
+        echo "No bookings found for the given date range.\n";
+    }
+}
+
+// Display the calendar
+displayCalendar(2025, 1);
+
+// Display the bookings
+displayBookings("2025-01-01", "2025-01-31");
 ?>
